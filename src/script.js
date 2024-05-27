77fbe03d8782f7ce62cdad6a56c306fbacf49c97
HUBSlover <ivanled2002@gmail.com>
2024-05-27 10:35:42 +0300

    5

---
+++ src/script.js
@@ -0,0 +1,195 @@
+const questions = [
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	},
+	{
+		question:
+			'An interface design application that runs in the browser with team-based collaborative design projects',
+		answers: [
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: true },
+			{ text: 'Figma', correct: false },
+			{ text: 'Figma', correct: false }
+		]
+	}
+]
+
+let currentQuestionIndex = 0
+let score = 0
+let answerChosen = false
+
+function startTheQuizFunction() {
+	document.getElementById('startCircleDiv').style.display = 'none'
+	document.getElementById('stepsFrame').style.display = 'block'
+	document.getElementById('questionContainer').style.display = 'block'
+	document.getElementById('answersBox').style.display = 'grid'
+	document.getElementById('questionContainer').style.display = 'block'
+	document.getElementById('submitButton').style.display = 'block'
+	document.getElementById('submitText').style.display = 'block'
+
+	for (let i = 1; i < 5; i++) {
+		document.getElementById(`answerButtonDefault${i}`).style.display = 'block'
+	}
+
+	document.getElementById('stepNumberEllipse1').className =
+		'stepNumberEllipseCurrent'
+	document.getElementById('stepNumber1').className = 'stepNumberCurrent'
+
+	for (let i = 2; i < 11; i++) {
+		document.getElementById(`stepNumberEllipse${i}`).className =
+			'stepNumberEllipseUpcoming'
+		document.getElementById(`stepNumber${i}`).className = 'stepNumberUpcoming'
+	}
+
+	showQuestion()
+}
+
+function showQuestion() {
+	let currentQuestion = questions[currentQuestionIndex]
+	let questionNo = currentQuestionIndex + 1
+	document.getElementById('questionText').innerHTML = currentQuestion.question
+}
+
+function clickAnswerButton1() {
+	document.getElementById('answerButtonDefault1').className =
+		'answerButtonSelected'
+	document.getElementById('answerOptionText1').className =
+		'answerOptionTextSelected'
+	document.getElementById('submitButton').className = 'submitButtonWorking'
+}
+
+function clickAnswerButton2() {
+	document.getElementById('answerButtonDefault2').className =
+		'answerButtonSelected'
+	document.getElementById('answerOptionText2').className =
+		'answerOptionTextSelected'
+}
+
+function clickAnswerButton3() {
+	document.getElementById('answerButtonDefault3').className =
+		'answerButtonSelected'
+	document.getElementById('answerOptionText3').className =
+		'answerOptionTextSelected'
+}
+
+function clickAnswerButton4() {
+	document.getElementById('answerButtonDefault4').className =
+		'answerButtonSelected'
+	document.getElementById('answerOptionText4').className =
+		'answerOptionTextSelected'
+}
+
+function proceedToTheNextQuestion() {
+	showQuestion()
+
+	for (let i = 1; i < 5; i++) {
+		document.getElementById(`answerButtonDefault${i}`).className =
+			'answerButtonDefault'
+		document.getElementById(`answerOptionText${i}`).className =
+			'answerOptionText'
+	}
+}
+
+function checkAnswer() {
+	for (let i = 0; i < 4; i++) {
+		if (
+			(document.getElementById(`answerButtonDefault${i + 1}`).className =
+				'answerButtonSelected')
+		) {
+			if (true === questions[currentQuestionIndex].answers[i].correct) {
+				score++
+
+				proceedToTheNextQuestion()
+			}
+		}
+
+		break
+	}
+}
