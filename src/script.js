0488412f5ebbfe71190edbcce72a85649dc4f544
HUBSlover <ivanled2002@gmail.com>
2024-05-28 13:34:10 +0300

    complete

--- src/script.js
+++ src/script.js
@@ -104,6 +104,36 @@ const questions = [
 let currentQuestionIndex = 0
 let score = 0
 let answerChosen = false
+let currentQuestion
+
+document.getElementById('scoreCircle').style.display = 'none'
+document.getElementById('startAgainDot').style.display = 'none'
+
+let answerButtonDefault1 = document.getElementById('answerButtonDefault1')
+let answerButtonDefault2 = document.getElementById('answerButtonDefault2')
+let answerButtonDefault3 = document.getElementById('answerButtonDefault3')
+let answerButtonDefault4 = document.getElementById('answerButtonDefault4')
+let answerOptionText1 = document.getElementById('answerOptionText1')
+let answerOptionText2 = document.getElementById('answerOptionText2')
+let answerOptionText3 = document.getElementById('answerOptionText3')
+let answerOptionText4 = document.getElementById('answerOptionText4')
+let submitButton = document.getElementById('submitButton')
+let submitText = document.getElementById('submitText')
+let nextButton = document.getElementById('nextButton')
+let nextButtonText = document.getElementById('nextButtonText')
+
+answerButtonDefault1.addEventListener('click', () => clickAnswerButton1())
+answerButtonDefault2.addEventListener('click', () => clickAnswerButton2())
+answerButtonDefault3.addEventListener('click', () => clickAnswerButton3())
+answerButtonDefault4.addEventListener('click', () => clickAnswerButton4())
+answerOptionText1.addEventListener('click', () => clickAnswerButton1())
+answerOptionText2.addEventListener('click', () => clickAnswerButton2())
+answerOptionText3.addEventListener('click', () => clickAnswerButton3())
+answerOptionText4.addEventListener('click', () => clickAnswerButton4())
+submitButton.addEventListener('click', () => checkAnswer())
+submitText.addEventListener('click', () => checkAnswer())
+nextButton.addEventListener('click', () => proceedToTheNextQuestion())
+nextButtonText.addEventListener('click', () => proceedToTheNextQuestion())
 
 function startTheQuizFunction() {
 	document.getElementById('startCircleDiv').style.display = 'none'
@@ -132,19 +162,20 @@ function startTheQuizFunction() {
 }
 
 function showQuestion() {
-	let currentQuestion = questions[currentQuestionIndex]
-	let questionNo = currentQuestionIndex + 1
-	document.getElementById('questionText').innerHTML = currentQuestion.question
+	if (questions[currentQuestionIndex]) {
+		currentQuestion = questions[currentQuestionIndex]
+		document.getElementById('questionText').innerHTML = currentQuestion.question
+	}
 }
 
 function clickAnswerButton1() {
-	if (answerChosen === false) {
+	if (answerChosen == false) {
 		document.getElementById('answerButtonDefault1').className =
 			'answerButtonSelected'
 		document.getElementById('answerOptionText1').className =
 			'answerOptionTextSelected'
 		document.getElementById('submitButton').className = 'submitButtonWorking'
-		answerChosen = 'true'
+		answerChosen = true
 	}
 }
 
@@ -155,59 +186,92 @@ function clickAnswerButton2() {
 		document.getElementById('answerOptionText2').className =
 			'answerOptionTextSelected'
 		document.getElementById('submitButton').className = 'submitButtonWorking'
-		answerChosen = 'true'
+		answerChosen = true
 	}
 }
 
 function clickAnswerButton3() {
-	if (answerChosen === false) {
+	if (answerChosen == false) {
 		document.getElementById('answerButtonDefault3').className =
 			'answerButtonSelected'
 		document.getElementById('answerOptionText3').className =
 			'answerOptionTextSelected'
 		document.getElementById('submitButton').className = 'submitButtonWorking'
-		answerChosen = 'true'
+		answerChosen = true
 	}
 }
 
 function clickAnswerButton4() {
-	if (answerChosen === false) {
+	if (answerChosen == false) {
 		document.getElementById('answerButtonDefault4').className =
 			'answerButtonSelected'
 		document.getElementById('answerOptionText4').className =
 			'answerOptionTextSelected'
 		document.getElementById('submitButton').className = 'submitButtonWorking'
-		answerChosen = 'true'
+		answerChosen = true
 	}
 }
 
 function proceedToTheNextQuestion() {
+	currentQuestionIndex++
 	showQuestion()
 
-	for (let i = 1; i < 5; i++) {
-		document.getElementById(`answerButtonDefault${i}`).className =
-			'answerButtonDefault'
-		document.getElementById(`answerOptionText${i}`).className =
-			'answerOptionText'
+	console.log(currentQuestionIndex)
+	if (currentQuestionIndex === 10) {
+		document.getElementById('answersBox').style.display = 'none'
+		document.getElementById('stepsFrame').style.display = 'none'
+		document.getElementById('additionalContainer').style.display = 'none'
+		document.getElementById('stepsLine').style.display = 'none'
+		document.getElementById('stepsNumbersFrame').style.display = 'none'
+		document.getElementById('questionContainer').style.display = 'none'
+		document.getElementById('questionBox').style.display = 'none'
+		document.getElementById('questionText').style.display = 'none'
+		document.getElementById('nextButton').style.display = 'none'
+		document.getElementById('nextButtonText').style.display = 'none'
+
+		for (let i = 1; i < 5; i++) {
+			document.getElementById(`answerButtonDefault${i}`).style.display = 'none'
+			document.getElementById(`answerOptionText${i}`).style.display = 'none'
+		}
+
+		for (let i = 1; i < 11; i++) {
+			document.getElementById(`stepNumberEllipse${i}`).style.display = 'none'
+			document.getElementById(`stepNumber${i}`).style.display = 'none'
+		}
+
+		document.getElementById('scoreText').style.display = 'block'
+		document.getElementById('scoreCircle').style.display = 'block'
+		document.getElementById('startAgainDot').style.display = 'flex'
+		document.getElementById('startAgainText').style.display = 'block'
+
+		document.getElementById('scoreText').innerHTML = `Your score is ${score}!`
+	} else {
+		for (let i = 1; i < 5; i++) {
+			document.getElementById(`answerButtonDefault${i}`).className =
+				'answerButtonDefault'
+			document.getElementById(`answerOptionText${i}`).className =
+				'answerOptionText'
+		}
+
+		document.getElementById('submitButton').style.display = 'block'
+		document.getElementById('submitText').style.display = 'block'
+		document.getElementById('nextButton').style.display = 'none'
+		document.getElementById('nextButtonText').style.display = 'none'
+
+		for (let i = 0; i < currentQuestionIndex + 1; i++) {
+			document.getElementById(`stepNumberEllipse${i + 1}`).className =
+				'stepNumberEllipse'
+			document.getElementById(`stepNumber${i + 1}`).className = 'stepNumber'
+		}
+
+		document.getElementById(
+			`stepNumberEllipse${currentQuestionIndex + 1}`
+		).className = 'stepNumberEllipseCurrent'
+		document.getElementById(`stepNumber${currentQuestionIndex + 1}`).className =
+			'stepNumberCurrent'
+
+		answerChosen = false
 	}
-
-	document.getElementById('submitButton').style.display = 'block'
-	document.getElementById('submitText').style.display = 'block'
-	document.getElementById('nextButton').style.display = 'none'
-	document.getElementById('nextButtonText').style.display = 'none'
-
-	for (let i = 0; i < currentQuestionIndex + 1; i++) {
-		document.getElementById(`stepNumberEllipse${i + 1}`).className =
-			'stepNumberEllipse'
-		document.getElementById(`stepNumber${i + 1}`).className = 'stepNumber'
-	}
-
-	console.log(`stepNumberEllipse${currentQuestionIndex + 2}`)
-	document.getElementById(
-		`stepNumberEllipse${currentQuestionIndex + 2}`
-	).className = 'stepNumberEllipseCurrent'
-	document.getElementById(`stepNumber${currentQuestionIndex + 2}`).className =
-		'stepNumberCurrent'
 }
 
 function checkAnswer() {
@@ -216,7 +280,7 @@ function checkAnswer() {
 			document.getElementById(`answerButtonDefault${i + 1}`).className ===
 			'answerButtonSelected'
 		) {
-			if (true === questions[currentQuestionIndex].answers[i].correct) {
+			if (questions[currentQuestionIndex].answers[i].correct === true) {
 				score++
 			}
 		}
@@ -246,6 +310,8 @@ function checkAnswer() {
 	document.getElementById('submitText').style.display = 'none'
 	document.getElementById('nextButton').style.display = 'block'
 	document.getElementById('nextButtonText').style.display = 'block'
+}
 
-	console.log(currentQuestionIndex)
+function restart() {
+	location.reload()
 }
